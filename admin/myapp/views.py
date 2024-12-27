7bdd06de158e9ecc716ffe950aeddbfd47fd3ffe
Seung-hwan285 <ghks123285@gmail.com>
2024-12-27 12:09:24 +0900

    add : get_list

--- admin/myapp/views.py
+++ admin/myapp/views.py
@@ -7,9 +7,10 @@ from djongo import models
 #----------------------------------------
 from pymongo import MongoClient
 
-from .models import SampleDetails
 
-import pprint
+from pprint import pprint
+
+from django.http import JsonResponse
 
 my_client = MongoClient('localhost', 27017)
 
@@ -34,7 +35,7 @@ medicine_1 = {
 # med_detatils = collection_name.find({})
 
 
-# [x] : 전체 데이터 출력
+# [] : 전체 데이터 출력
 # [] : 이름만 출력
 # [] : 이름,카테고리만출력
 
@@ -43,24 +44,29 @@ medicine_1 = {
 # [] : 버튼 하나만들고 누르면 이름,카테고리만 출력
 
 
-def index(request):
-    
-    
-    
-    
-    return HttpResponse("<h1>my home<u>Django App</u> project!</h1>")
+def index(request):    
+    return render(request,'view.html')
+    # return HttpResponse("<h1>my home<u>Django App</u> project!</h1>")
 
 
 def get_list(request):
     
     print(request.method,'-----------')    
-    data = collection_name.find({"common_name": "Paracetamol"})
-
-    for r in data:
-        pprint.pprint(r)
+    # Convert cursor to list
+    # 몽고디비는 항상 리스트를 convert 해줘서 던져줘야한다
+    data = list(collection_name.find({}))
+  
+    
+    try:
+        for p in data:
+            pprint(p)
+        return render(request,'list.html', {"data" :data})
+    
+    except Exception as e:
+        return HttpResponse(f"<h1>{str(e)}</h1>")
     
     
-    return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
+    # return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
 
 def get_name_list(request):
     
@@ -77,3 +83,4 @@ def get_name_list(request):
     
         
     return HttpResponse("<h1>Hello and welcome to my first <u>Django App</u> project!</h1>")
+
